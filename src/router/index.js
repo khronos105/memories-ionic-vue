f5ddb69360b907902d123eb5b03c311f1a6429ce
Roma Bilibov <romabilibov@gmail.com>
2022-05-02 14:37:38 +0200

    Cleaned default component

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
 
 const routes = [
   {
     path: '/',
     redirect: '/home'
   },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
 ]
 
 const router = createRouter({
